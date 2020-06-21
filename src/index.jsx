8951115db9b4fd686de8366aa291fe37b5dd55cc
takaykuki0310 <t.k.y079c@gmail.com>
2020-06-21 15:27:39 +0900

    Initial Commit

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import CalendarBoard from "./components/CalendarBoard/container";
+
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+
+dayjs.locale("ja");
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import rootReducer from './redux/rootReducer'
+
+const store = createStore(rootReducer)
+
+const App = () =>
+<Provider store={store}>
+  <CalendarBoard />
+</Provider>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
